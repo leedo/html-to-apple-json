9880a0da2581b417356033d5a5b949ecae73b94e
Lee Aylward <lee@laylward.com>
2015-09-04 00:39:31 -0400

    white list of tags for body elements
    other stuff won't translate well in markdown anyhow

--- lib/HtmlToApple/Component/Body.pm
+++ lib/HtmlToApple/Component/Body.pm
@@ -4,6 +4,10 @@ use Moo;
 
 extends "HtmlToApple::Component::Markdown";
 
+sub allowed_tags {
+  qw{p ol ul li blockquote pre code a hr b em strong i}
+}
+
 sub concat {
   my ($self, $comp) = @_;
   push @{$self->html}, @{$comp->html};
